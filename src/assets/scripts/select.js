959b822f499c8d56d9538fe01340040e2abf9303
szatolokin <staszatolokin28@gmail.com>
2021-02-08 06:05:15 +0300

    fix custom text-only select in filter

--- src/assets/scripts/select.js
+++ src/assets/scripts/select.js
@@ -5,10 +5,16 @@ function formatState(state) {
 	if (!state.id) {
 		return state.text;
 	}
-
-	const $state = $(
-		'<span><img src="' + state.element.dataset.img + '" class="img-flag" /> ' + state.text + '</span>'
-	);
+	let $state;
+	if (state.element.dataset.img !== undefined) {
+		$state = $(
+			'<span><img src="' + state.element.dataset.img + '" class="img-flag" /> ' + state.text + '</span>'
+		);
+	} else {
+		$state = $(
+			'<span>' + state.text + '</span>'
+		);
+	}
 	return $state;
 }
 
@@ -17,9 +23,17 @@ function formatStateSelection(state, e) {
 		return state.text;
 	}
 	const title = $(e.prevObject).closest('.select2').prev().data('title');
-	const $state = $(
-		'<span><img class="img-flag" /> <span></span></span>'
-	);
+	
+	let $state;
+	if (state.element.dataset.img !== undefined) {
+		$state = $(
+			'<span><img class="img-flag" /> <span></span></span>'
+		);
+	} else {
+		$state = $(
+			'<span><span></span></span>'
+		);
+	}
 
 	$state.find('span').html(`<span class="title">${title}</span>${state.text}`);
 	$state.find('img').attr('src', state.element.dataset.img);
@@ -31,7 +45,8 @@ $('.select-template').select2({
 	selectOnClose: true,
 	templateResult: formatState,
 	templateSelection: formatStateSelection,
-	minimumResultsForSearch: Infinity
+	minimumResultsForSearch: Infinity,
+	theme: $(this).data('theme')
 });
 
 $('.select-template').on('select2:select', function(e) {
@@ -58,73 +73,6 @@ $('.select-template').on('select2:closing', function(e) {
 				});
 				$('.select-template').removeClass('closing');
 			}, 500);
-		});
-	}, 0);
-});
-
-// main
-
-function formatStateMain(state) {
-	if (!state.id) {
-		return state.text;
-	}
-
-	const $state = $(
-		'<span>' + state.text + '</span>'
-	);
-	return $state;
-}
-
-function formatStateSelectionMain(state, e) {
-	if (!state.id) {
-		return state.text;
-	}
-	const title = $(e.prevObject).closest('.select2').prev().data('title');
-	const $state = $(
-		'<span><span></span></span>'
-	);
-
-	$state.find('span').html(`<span class="title">${title}</span>${state.text}`);
-
-	return $state;
-}
-
-$('.select-main').select2({
-	selectOnClose: true,
-	templateResult: formatStateMain,
-	templateSelection: formatStateSelectionMain,
-	minimumResultsForSearch: Infinity,
-	theme: 'main'
-});
-
-$('.map__select').select2({
-	selectOnClose: true,
-	templateResult: formatStateMain,
-	templateSelection: formatStateSelectionMain,
-	minimumResultsForSearch: Infinity,
-	theme: 'main'
-});
-
-$('.select-main').on('select2:open', function() {
-	$('.select2-dropdown').hide();
-	setTimeout(function() {
-		$('.select2-dropdown').slideDown({ duration: 500, easing: 'easeInOutCubic' });
-	}, 200);
-});
-$('.select-main').on('select2:closing', function(e) {
-	e.preventDefault();
-	setTimeout(function() {
-		$('.select2').addClass('closing');
-		$('.select2-dropdown').slideUp(500, function() {
-			$('.select-main').select2('destroy');
-			$('.select-main').select2({
-				selectOnClose: true,
-				templateResult: formatStateMain,
-				templateSelection: formatStateSelectionMain,
-				minimumResultsForSearch: Infinity,
-				theme: 'main'
-			});
-			$('.select-main').removeClass('closing');
 		});
 	}, 0);
 });
