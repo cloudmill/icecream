588a88c37bfb03697f5bb76b6694c3e730c183bb
szatolokin <staszatolokin28@gmail.com>
2021-02-20 14:33:30 +0300

    fixes

--- src/assets/scripts/input.js
+++ src/assets/scripts/input.js
@@ -2,23 +2,23 @@ import Inputmask from 'inputmask';
 
 Inputmask({'mask': '+7 (999) 999-99-99'}).mask('input[name=phone]');
 
+// валидация
+
 export function validateEmail(email) {
 	const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(email);
 }
-
 export function validatePhone(phone) {
 	const re = /^(\+7)[\s\-]\(?[0-9]{3}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
 	return re.test(phone);
 }
-
-
 export function validateField(element, event) {
 	const isRequired = element.attr('required');
 	const errorBlock = '.control-error';
 	const checkbox = element.attr('type') === 'checkbox';
 	const value = event;
 	const tmpval = element.val();
+	
 	let result;
 
 	if (tmpval === '') {
@@ -29,6 +29,7 @@ export function validateField(element, event) {
 		result = true;
 	}
 
+	// email & phone
 	if (value) {
 		if (element.prop('type') === 'email') {
 			if (validateEmail(value)) {
@@ -57,52 +58,51 @@ export function validateField(element, event) {
 		}
 	}
 
+	// checkbox
 	if (checkbox && isRequired) {
-	  console.log('form validation: ', element.prop('checked'));
 		if (element.prop('checked')) {
-			// element.closest('.checkbox').removeClass('error');
+			// element.closest('.checkbox').removeClass('error'); т.к checkbox не стилизуется при ошибке - выводится только сообщение об ошибке
 			element.closest('.checkbox').find(errorBlock).text('');
 			result = true;
 		} else {
-			// element.closest('.checkbox').addClass('error');
+			// element.closest('.checkbox').addClass('error'); т.к checkbox не стилизуется при ошибке - выводится только сообщение об ошибке
 			element.closest('.checkbox').find(errorBlock).text('Для отправки сообщения необходимо согласие');
 			result = false;
 		}
 	}
+
+	// required input
 	if (!value && isRequired) {
 		element.closest('.input').addClass('error');
 		element.closest('.input').find(errorBlock).text('Пожалуйста, заполните поле');
 		result = false;
 	}
+
+	// file
 	if (element.prop('type') === 'file') {
 		result = true;
 	}
+
 	return result;
 }
-/*
-$(document).on('blur', '.input input, .input textarea', function(event) {
-	validateField($(this), event.target.value);
-});
-$(document).on('change', '.checkbox input', function(event) {
-	validateField($(this), event.target.value);
-});
-*/
 
-
-// form
-
-// eslint-disable-next-line consistent-return
-$('.form--js').click(function(e) {
+$('.form--js').on('click', function(e) {
 	e.preventDefault();
+	
+	// валидация каждого поля формы
 	const result = [];
 	$(this).closest('form').find('input, textarea').each(function() {
 		const input = $(this)[0];
 		result.push(validateField($(this), input.value));
 	});
+
+	// результат валидации формы
 	const isNONValid = result.includes(false);
 	if (isNONValid) {
 		return false;
 	}
+
+	// сбор данных формы
 	const form = $(this).closest('form');
 	const name = form.find('input[name=name]');
 	const email = form.find('input[name=email]');
@@ -124,6 +124,8 @@ $('.form--js').click(function(e) {
 		};
 	}
 
+	// открытие формы ответа
+	// контакты
 	curForm.closest('.form-inner').css('opacity', 0).next().slideDown(500).css('display', 'flex');
 	$('.form-back--js').one('click', function() {
 		const form = $(this).closest('form');
@@ -132,7 +134,7 @@ $('.form--js').click(function(e) {
 		$(this).closest('.form-send').hide().prev().css('opacity', 1);
 		return false;
 	});
-
+	// подписка, формы боковой панели
 	$('.form-response').addClass('shown activated');
 	$('.reset--js').one('click', function() {
 		$('.form-response').removeClass('shown');
@@ -141,19 +143,4 @@ $('.form--js').click(function(e) {
 		}, 500);
 		return false;
 	});
-
-	/*
-	$.ajax({
-		type: 'POST',
-		url: url,
-		dataType: 'json',
-		data: data,
-		success: function(a) {
-			console.log(a.success);
-			if (a.success === true) {
-				curForm.closest('.form-inner').css('opacity', 0).next().slideDown(500).css('display', 'flex');
-			}
-		}
-	});
-	*/
 });
