e4096a215ce34834fce79d3a5406054d5817a300
Anna <viirall@yandex.ru>
2021-03-02 17:21:35 +0200

    fix

--- src/assets/scripts/ref.js
+++ src/assets/scripts/ref.js
@@ -28,13 +28,15 @@ $(document).on('input', 'input[type="range"]', function(e) {
 	const regexp = /\B(?=(\d{3})+(?!\d))/g;
 	const values = $(this).closest('.referral__calc-block').find('output');
 	const res = values[0].innerText * values[1].innerText * 0.1;
-	$('.result__block').removeClass('active');
-	if (e.target.value < 250) {
-		$('.result__block:first-child').addClass('active');
-	} else if (parseInt(e.target.value, 10) >= 250 && parseInt(e.target.value, 10) < 5000) {
-		$('.result__block:nth-child(2)').addClass('active');
-	} else {
-		$('.result__block:nth-child(3)').addClass('active');
+	if(e.target.id === 'summ') {
+		$('.result__block').removeClass('active');
+		if (e.target.value < 250) {
+			$('.result__block:first-child').addClass('active');
+		} else if (parseInt(e.target.value, 10) >= 250 && parseInt(e.target.value, 10) < 5000) {
+			$('.result__block:nth-child(2)').addClass('active');
+		} else {
+			$('.result__block:nth-child(3)').addClass('active');
+		}
 	}
 
 	$('.result__block--js .result .sum').text(res.toString().replace(regexp, ' '));
