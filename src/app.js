c45125ddf658fdd08440b250a4a7d799fa4fa8d0
szatolokin <staszatolokin28@gmail.com>
2021-03-02 11:03:22 +0300

    ### Test build

--- src/app.js
+++ src/app.js
@@ -284,16 +284,3 @@ $(() => {
 		}, 500);
 	}
 });
-
-{
-	const testElement = document.createElement('div');
-	$(testElement).css('position', 'fixed')
-		.css('top', '0')
-		.css('bottom', '0')
-		.css('left', '0')
-		.css('right', '0')
-		.css('border', '10px dashed black')
-		.css('z-index', '100000000');
-	$(document.body).append(testElement);
-}
-
