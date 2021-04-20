b9aa5dad8147a6f4589b14c998604d3678d2ffb8
Alex <a.yugai@dterra.eu>
2021-04-20 18:41:37 +0300

    update

--- src/assets/scripts/backend.js
+++ src/assets/scripts/backend.js
@@ -26,7 +26,7 @@ function showMore() {
                 ajax: true,
             }
         }
-        
+
         if (url !== undefined) {
             pagenav.remove();
             $.ajax({
@@ -37,7 +37,7 @@ function showMore() {
                 success: function (data) {
                     let itemsResponse = $(data).find('[data-type=item]'),
                         pagenavResponse = $(data).find('[data-type=pagenav_block]');
-                    
+
                     itemsContainer.append(itemsResponse);
                     itemsContainer.after(pagenavResponse);
                 }
@@ -47,17 +47,17 @@ function showMore() {
 }
 
 function changeFilterCatalog() {
-    kind = null,
-    flavors = null,
-    data = null,
-    container = null,
-    itemsBlock = null,
-    filterBlock = null,
-    pagenavBlock = null,
-    filterSelects = null,
-    filterOptions = null,
-    selectKind = null,
-    selectFlavors = null;
+    let kind = null,
+      flavors = null,
+      data = null,
+      container = null,
+      itemsBlock = null,
+      filterBlock = null,
+      pagenavBlock = null,
+      filterSelects = null,
+      filterOptions = null,
+      selectKind = null,
+      selectFlavors = null;
 
     $('[data-type=reset_filters_catalog]').on('click', function () {
         container = $(this).parents(['data-type=catalog_container']),
@@ -68,7 +68,7 @@ function changeFilterCatalog() {
         filterOptions = container.find('[data-type=change_filter_catalog] option'),
         selectKind = container.find('[data-type-title=kind]'),
         selectFlavors = container.find('[data-type-title=flavors]');
-        
+
         data = {
             ajax: true,
             kind: '',
@@ -92,12 +92,12 @@ function changeFilterCatalog() {
             if ($(this).attr('data-type-title') == 'kind') {
                 kind = $(this).val();
             }
-            
+
             if ($(this).attr('data-type-title') == 'flavors') {
                 flavors = $(this).val();
             }
         });
-        
+
         data = {
             ajax: true,
             kind: kind,
@@ -136,19 +136,19 @@ function catalogFilterAjax(data) {
 }
 
 function changeFilterRecipes() {
-    type = null,
-    products = null,
-    time = null,
-    data = null,
-    container = null,
-    appendItems = null,
-    itemsBlock = null,
-    filterSelects = null,
-    filterOptions = null,
-    typeBlockSelect = null,
-    productsBlockSelect = null,
-    timeBlockSelect = null,
-    pagenavBlock = null;
+    let type = null,
+      products = null,
+      time = null,
+      data = null,
+      container = null,
+      appendItems = null,
+      itemsBlock = null,
+      filterSelects = null,
+      filterOptions = null,
+      typeBlockSelect = null,
+      productsBlockSelect = null,
+      timeBlockSelect = null,
+      pagenavBlock = null;
 
     $('[data-type=reset_filters_recipes]').on('click', function () {
         container = $(this).parents(['data-type=container_recipes']),
@@ -160,14 +160,14 @@ function changeFilterRecipes() {
         productsBlockSelect = container.find('[data-title-type=products]'),
         timeBlockSelect = container.find('[data-title-type=time]'),
         pagenavBlock = container.find('[data-type=pagenav_block]');
-        
+
         data = {
             ajax: true,
             type: '',
             products: '',
             time: '',
         }
-        
+
         recipesFilterAjax(data);
     });
 
@@ -186,11 +186,11 @@ function changeFilterRecipes() {
             if ($(this).attr('data-title-type') == 'type') {
                 type = $(this).val();
             }
-            
+
             if ($(this).attr('data-title-type') == 'products') {
                 products = $(this).val();
             }
-            
+
             if ($(this).attr('data-title-type') == 'time') {
                 time = $(this).val();
             }
@@ -202,7 +202,7 @@ function changeFilterRecipes() {
             products: products,
             time: time,
         };
-        
+
         recipesFilterAjax(data);
     });
 }
@@ -228,7 +228,7 @@ function recipesFilterAjax(data) {
             timeBlockSelect.append(selectTimeResponse);
             appendItems.append(itemsResponse);
             itemsResponse.after(pagenavResponse);
-            
+
             filterSelects.each(function () {
                 $(this).val($(this).find('[selected]').val()).trigger('change');
             });
@@ -242,10 +242,10 @@ function filterFestMonth() {
         month = $(this).attr('data-month'),
         itemsContainer = container.find('[data-type=items-container]'),
         dateSelect = container.find('[data-type=date-filter-select]');
-  
+
       itemsContainer.empty();
       dateSelect.empty();
-  
+
       $.ajax({
         type: 'post',
         url: window.location.pathname,
@@ -256,31 +256,31 @@ function filterFestMonth() {
         success: function(data) {
           let dateSelectResponse = $(data).find('[data-type=date-filter-select]').children(),
             itemsContainerResponse = $(data).find('[data-type=items-container]').children();
-  
+
           dateSelect.append(dateSelectResponse);
           itemsContainer.append(itemsContainerResponse);
-  
+
           dateSelect.each(function () {
             $(this).val($(this).find('[selected]').val()).trigger('change');
           });
-  
+
           initSwiper();
         }
       });
     });
   }
-  
+
   function filterFestDate() {
     $('[data-type=date-filter-select]').on('select2:select', function() {
       let container = $(this).parents('[data-type=fest-foto-container]'),
         month = container.find('[data-type=select-month].active').attr('data-month'),
         itemsContainer = container.find('[data-type=items-container]'),
         dateVal = $(this).val();
-  
+
       console.log(month);
-  
+
       itemsContainer.empty();
-  
+
       $.ajax({
         type: 'post',
         url: window.location.pathname,
@@ -291,12 +291,11 @@ function filterFestMonth() {
         },
         success: function(data) {
           let itemsContainerResponse = $(data).find('[data-type=items-container]').children();
-  
+
           itemsContainer.append(itemsContainerResponse);
-  
+
           initSwiper();
         }
       });
     });
   }
-  
