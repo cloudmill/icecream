850aa37e3bfc4471640fc0d79f8f78122fb21b19
szatolokin <staszatolokin28@gmail.com>
2021-06-11 15:52:39 +0300

    Change favicon .png -> .ico

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -177,7 +177,7 @@ module.exports = env => {
 		plugins: [
 			new CopyWebpackPlugin({
 				patterns: [
-					{from: 'assets/images/favicons/favicon.png', to: 'assets/favicon.png'},
+					{from: 'assets/images/favicons/favicon.ico', to: 'assets/favicon.ico'},
 					{from: 'assets/images', to: 'assets/images'},
 					{from: 'assets/fonts', to: 'assets/fonts'},
 				]
